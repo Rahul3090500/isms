f4a6c1d8f5f906db92c5df9d5779afb086f8a916
Rahul <109056697+Rahul3090500@users.noreply.github.com>
2024-03-30 10:31:19 +0530

    Update SideNav.tsx

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -12,36 +12,38 @@ const SideNav = ({
   // Local state to track the selected item
   const [activeItem, setActiveItem] = useState<string | null>(null);
 
-  const handleItemClick = (item: any) => {
-    if (!['Summary', 'Sentiments', 'Classification','AI Response'].includes(item.text) || videoSummary) {
-      setActiveItem(item.content); // Update the local state
-      onItemSelect(item.content); // Notify the parent component
+  const handleItemClick = (item:any) => {
+    if (!['Summary', 'Sentiments', 'Classification', 'AI Response'].includes(item.text) || videoSummary) {
+      setActiveItem(item.text); // Now using item.text to set the active state
+      console.log(`Item selected: ${item.text}`); // This will log the text of the item
+
+      // Notify the parent component, if needed, possibly using a different property like item.content
+      onItemSelect(item.content); 
       
       // Additional logic based on the item text
       if (item.text === 'AI Response') {
         setIsFileOpener(true);
-      }
-      if (item.text === 'Sentiments') {
+      } else if (item.text === 'Sentiments') {
         handleSentimentAnalysis();
-      }
-      if (item.text === 'Classification') {
+      } else if (item.text === 'Classification') {
         handleCommentClassifications();
       }
     }
   };
 
+
   return (
     <div className={classes.sidenav}>
       <div className={classes.header}>Isms</div>
       <div className={classes.content}>
-        {navItems.map((item: any, index: number) => (
+        {navItems.map((item:any, index:any) => (
           <div
             key={index}
             onClick={() => handleItemClick(item)}
-            className={`${classes.singleItem}  ${activeItem === item.content ? classes.selectedItem : ''} ${!videoSummary && ['Summary', 'Sentiments', 'Classification','AI Response'].includes(item.text) ? classes.disabled : ""}`}
+            className={`${classes.singleItem} ${activeItem === item.text ? classes.selectedItem : ''} ${!videoSummary && ['Summary', 'Sentiments', 'Classification', 'AI Response'].includes(item.text) ? classes.disabled : ""}`}
           >
             <span
-              className={`${classes.icon}  ${activeItem === item.content ? classes.iconSelect : ''} ${!videoSummary && ['Summary', 'Sentiments', 'Classification','AI Response'].includes(item.text) ? classes.iconDisabled : ""}`}
+              className={`${classes.icon} ${activeItem === item.text ? classes.iconSelect : ''} ${!videoSummary && ['Summary', 'Sentiments', 'Classification', 'AI Response'].includes(item.text) ? classes.iconDisabled : ""}`}
             >
               {item.icon}
             </span>
